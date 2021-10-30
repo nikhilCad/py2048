970b0a4e6b25f49fb630e93f8ddd4aaf823eeee2
nikhilcad <nikhilcaddilac@gmail.com>
2021-10-30 12:45:49 +0530

    Grid finalized

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # py2048
 2048 in command line, built in python
+
+Use W A S D to play the Game!
--- py2048.py
+++ py2048.py
@@ -8,15 +8,17 @@ gridSize = 4
 #       print("*"*blockSize, end=" ")
 #    print("")
 
-grid =  [[0]*gridSize]*gridSize
+# Use W A S D to play the Game!
+
+grid =  {(i,j):0 for i in range(gridSize) for j in range(gridSize) }
 
 def printGrid():
     for i in range(gridSize):
         for j in range(gridSize):      
-            lenofNum = len(str(grid[i][j]))
-            print(grid[i][j], (blockSize-lenofNum)*"*",sep = "",end =" ")
+            lenofNum = len(str(grid[i,j]))
+            print(grid[i,j], (blockSize-lenofNum)*"*",sep = "",end =" ")
         print("") 
 
-grid[random.randint(0,gridSize-1)][random.randint(0,gridSize-1)] = 2
+grid[random.randint(0,gridSize-1) , random.randint(0,gridSize-1)] = 2
 
 printGrid()
